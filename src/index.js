c6e17af7382e9fb4cd687fbc45d100edf2038ab6
System Administrator <root@Juricas-MacBook-Pro.local>
2019-12-21 22:52:08 +0100

    Starting development

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const main = () => {
+  const commands = () => {};
+};
+document.getElementById("start").addEventListener("click", () => {
+  document.getElementById("main").classList.add("started");
+});
